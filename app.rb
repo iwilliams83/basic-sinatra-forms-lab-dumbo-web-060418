56ba8de6cd909887f8b2f8542064e06c89f6874e
iwilliams83 <indirajamurath@gmail.com>
2018-06-26 11:21:03 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,9 +2,12 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
   get '/newteam' do
-    puts "I'm in the get. My params are #{params}"
     erb :newteam
   end
+
+  get '/team' do
+    puts "I'm in the get. My params are #{params}"
+  end 
 
   post '/team' do
     "I'm in the post. My params are #{params}"
